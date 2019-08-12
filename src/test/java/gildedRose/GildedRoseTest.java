b532171e119eebf6a16739c4791ece4cbbaccb14
TANGLO2 <1614468806@qq.com>
2019-08-13 00:34:37 +0800

    test: add test case for Backstage passes

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -40,7 +40,7 @@ public class GildedRoseTest {
 
 
     @Test
-    public void should_return_31_when_call_updateQuality_given_Aged_BrieSe_sellIn_10_quality_30() {
+    public void should_return_31_when_call_updateQuality_given_Aged_Brie_sellIn_10_quality_30() {
         Item item = new Item("Aged Brie", 10, 30);
         GildedRose gildedRose = new GildedRose(new Item[]{item});
 
@@ -50,7 +50,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_return_50_when_call_updateQuality_given_normal_item_sellIn_10_quality_50() {
+    public void should_return_50_when_call_updateQuality_given_Aged_Brie_sellIn_10_quality_50() {
         Item item = new Item("Aged Brie", 10, 50);
         GildedRose gildedRose = new GildedRose(new Item[]{item});
 
@@ -59,10 +59,8 @@ public class GildedRoseTest {
         assertEquals(50, item.quality);
     }
 
-
-
     @Test
-    public void should_return_same_when_call_updateQuality_given_normal_item_sellIn_10_quality_20() {
+    public void should_return_same_when_call_updateQuality_given_Sulfuras_sellIn_10_quality_20() {
         Item item = new Item("Sulfuras, Hand of Ragnaros", 10, 20);
         GildedRose gildedRose = new GildedRose(new Item[]{item});
 
@@ -71,4 +69,47 @@ public class GildedRoseTest {
         assertEquals(10, item.sellIn);
         assertEquals(20, item.quality);
     }
+
+
+    @Test
+    public void should_return_22_when_call_updateQuality_given_Backstage_passes_sellIn_8_quality_20(){
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 8, 20);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(22, item.quality);
+    }
+
+    @Test
+    public void should_return_50_when_call_updateQuality_given_Backstage_passes_sellIn_8_quality_49() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 8, 49);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(50, item.quality);
+    }
+
+    @Test
+    public void should_return_33_when_call_updateQuality_given_Backstage_passes_sellIn_5_quality_30() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 30);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(33, item.quality);
+    }
+
+
+    @Test
+    public void should_return_0_when_call_updateQuality_given_Backstage_passes_sellIn_0_quality_20() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(0, item.quality);
+    }
+
 }
