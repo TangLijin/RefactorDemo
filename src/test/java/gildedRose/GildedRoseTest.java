eb1e2a6f1e75124eb06a1a54589dd7a48b45aaab
TANGLO2 <1614468806@qq.com>
2019-08-13 00:07:11 +0800

    test: add test cases for"Once the sell by date has passed, Quality degrades twice as fast " and "The Quality of an item is never negative"

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -16,6 +16,26 @@ public class GildedRoseTest {
         assertEquals(9,item.quality);
         assertEquals(9,item.sellIn);
     }
-    
+
+
+    @Test
+    public void should_return_8_when_call_updateQuality_given_normal_item_sellIn_0_quality_10() {
+        Item item = new Item("Normal Item", 0, 10);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(8, item.quality);
+    }
+
+    @Test
+    public void should_return_0_when_call_updateQuality_given_normal_item_sellIn_10_quality_0(){
+        Item item = new Item("Normal Item", 10, 0);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(0, item.quality);
+    }
 
 }
